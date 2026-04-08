9c4659f36b4c8da973512e006f7ed7c8c4a90f90
lheintzmann1 <lheintzmann1@disroot.org>
2026-04-08 20:46:44 +0200

    cpp: last-will

---
+++ cpp/last-will/CMakeFiles/last-will.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/last-will.dir/last_will_test.cpp.obj"
+  "CMakeFiles/last-will.dir/last_will_test.cpp.obj.d"
+  "CMakeFiles/last-will.dir/test/tests-main.cpp.obj"
+  "CMakeFiles/last-will.dir/test/tests-main.cpp.obj.d"
+  "last-will.exe"
+  "last-will.exe.manifest"
+  "last-will.pdb"
+  "liblast-will.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/last-will.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

8681e7386b16832c9baa2e8fc6805db1d61bde4d
lheintzmann1 <lheintzmann1@disroot.org>
2026-04-11 17:35:17 +0200

    cpp: freelancer-rates

---
+++ cpp/freelancer-rates/CMakeFiles/freelancer-rates.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/freelancer-rates.dir/freelancer_rates_test.cpp.obj"
+  "CMakeFiles/freelancer-rates.dir/freelancer_rates_test.cpp.obj.d"
+  "CMakeFiles/freelancer-rates.dir/test/tests-main.cpp.obj"
+  "CMakeFiles/freelancer-rates.dir/test/tests-main.cpp.obj.d"
+  "freelancer-rates.exe"
+  "freelancer-rates.exe.manifest"
+  "freelancer-rates.pdb"
+  "libfreelancer-rates.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/freelancer-rates.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
